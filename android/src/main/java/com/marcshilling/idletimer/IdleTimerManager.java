e34dfec679312834e3cbe911ce300f3a41c8b324
Marc Shilling <marcshilling@gmail.com>
2016-04-21 15:48:10 -0400

    lets see if this works

---
+++ android/src/main/java/com/marcshilling/idletimer/IdleTimerManager.java
@@ -0,0 +1,41 @@
+package com.discovery.DiscoveryVR.Modules.IdleTimer;
+
+import android.app.Activity;
+import android.view.WindowManager;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class IdleTimerManager extends ReactContextBaseJavaModule
+{
+
+    public IdleTimerManager(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "IdleTimerManager";
+    }
+
+    @ReactMethod
+    public void setIdleTimerDisabled(final boolean disabled) {
+
+        final Activity activity = this.getCurrentActivity();
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (disabled) {
+                        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    } else {
+                        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    }
+                }
+            });
+        }
+
+
+    }
+}
