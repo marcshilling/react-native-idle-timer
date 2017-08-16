daaa8e60b226e6648ef21ef2e572a5ed9dc5198f
Mark Miyashita <negativetwelve@gmail.com>
2017-08-16 16:35:41 -0700

    Add react-native-package to protect against bad installations

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict'
 
-var { NativeModules } = require('react-native')
+var { NativeModules, Platform } = require('react-native')
+var Package = require('react-native-package');
 
-module.exports = NativeModules.IdleTimerManager;
+module.exports = Package.create({
+  json: require('./package.json'),
+  nativeModule: NativeModules.IdleTimerManager,
+  enabled: Platform.select({
+    android: true,
+    ios: true,
+  }),
+  export: function(IdleTimer) {
+    return {
+      setIdleTimerDisabled: function(isDisabled) {
+        IdleTimer.setIdleTimerDisabled(isDisabled);
+      },
+    };
+  },
+});
