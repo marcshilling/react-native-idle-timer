25e87c31368af35ab642aafe8181782008ab268c
Johan Blomgren <j.blomgr@gmail.com>
2018-04-14 17:15:04 +0200

    UPDATES UI method should be called on main thread

--- ios/RNIdleTimer/IdleTimerManager.m
+++ ios/RNIdleTimer/IdleTimerManager.m
@@ -5,7 +5,9 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(setIdleTimerDisabled:(BOOL)disabled) {
-    [UIApplication sharedApplication].idleTimerDisabled = disabled;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [UIApplication sharedApplication].idleTimerDisabled = disabled;
+    });
 }
 
 @end
